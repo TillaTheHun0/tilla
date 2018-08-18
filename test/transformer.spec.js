31761f510e08319d1c8f129143f778ac4e082b31
Tyler Hall <0tillathehun0@gmail.com>
2018-08-18 15:59:37 -0400

    test (transformer): Add rest of transformer tests.

--- test/transformer.spec.js
+++ test/transformer.spec.js
@@ -8,4 +8,12 @@ describe('Transformer', () => {
   it('should throw err on passthrough when default flag is not set', tests.passthroughErr)
   it('should set to build_with by default and set builder', tests.buildWith)
   it('should throw err on build_with when default flag is not set', tests.buildWithErr)
+  it('should transform the object and ignore values not specified', tests.transformNoDefault)
+  it('should transform the object and specified default fields as specified', tests.transformWithDefault)
+  it('should transform the object and ignore values not specified in default attributes', tests.transformWithDefaultNoAttributes)
+  it('should transform the object and specified default fields with the default builder', tests.transformWithDefaultBuildWith)
+  it('should transform the object and default custom builder and ignore values not specified in default attributes',
+    tests.transformWithDefaultBuildWithNoAttributes)
+  it('should throw an error if no default masking is set', tests.transformWithDefaultNoMaskErr)
+  it('should extend the transformer and merge the maps', tests.extend)
 })
--- test/transformer/index.js
+++ test/transformer/index.js
@@ -2,7 +2,7 @@
 
 import { expect } from 'chai'
 
-import { Transformer, utils } from '../../src'
+import { Transformer, utils, FieldPermissionLvl } from '../../src'
 
 function byDefault () {
   let transformer = new Transformer({
@@ -70,10 +70,169 @@ function buildWithErr () {
   }
 }
 
+function transformNoDefault (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  })
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200 // field ignored by transformer
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(1)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.otherValue).to.be.equal(undefined)
+    done()
+  })
+}
+
+function transformWithDefault (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  }).byDefault(['otherValue']).PASSTHROUGH()
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(1)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.otherValue).to.be.equal(200)
+    done()
+  })
+}
+
+function transformWithDefaultNoAttributes (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  }).byDefault().PASSTHROUGH()
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200 // field ignored by transformer
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(1)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.otherValue).to.be.equal(undefined)
+    done()
+  })
+}
+
+function transformWithDefaultBuildWith (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  }).byDefault(['otherValue']).BUILD_WITH((instance, key) => {
+    return instance[key] + 1
+  })
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(1)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.otherValue).to.be.equal(201)
+    done()
+  })
+}
+
+function transformWithDefaultBuildWithNoAttributes (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  }).byDefault().BUILD_WITH((instance, key) => {
+    return instance[key] + 1
+  })
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(1)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.otherValue).to.be.equal(undefined)
+    done()
+  })
+}
+
+function transformWithDefaultNoMaskErr (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  }).byDefault(['otherValue']) // missing mask here
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200
+  }
+
+  transformer.transform(FieldPermissionLvl.PRIVATE, obj).catch(() => {
+    done()
+  })
+}
+
+function extend (done) {
+  let transformer = new Transformer({
+    value: utils.fieldDelegate('value').always().passthrough(),
+    secret: utils.fieldDelegate('secret').restrictToPrivate().passthrough()
+  })
+
+  let extension = transformer.extend({
+    value: utils.fieldDelegate('value').restrictToAdmin().passthrough(),
+    otherValue: utils.fieldDelegate('otherValue').always().buildWith((instance, key) => {
+      return instance[key] + 1
+    }),
+    computed: utils.fieldDelegate().always().buildWith(() => {
+      return 500
+    })
+  })
+
+  let obj = {
+    value: 1,
+    secret: 100,
+    otherValue: 200
+  }
+
+  extension.transform(FieldPermissionLvl.PRIVATE, obj).then((dto) => {
+    expect(dto.value).to.be.equal(undefined)
+    expect(dto.secret).to.be.equal(100)
+    expect(dto.computed).to.be.equal(500)
+    expect(dto.otherValue).to.be.equal(201)
+    done()
+  })
+}
+
 export {
   byDefault,
   passthrough,
   passthroughErr,
   buildWith,
-  buildWithErr
+  buildWithErr,
+  transformNoDefault,
+  transformWithDefault,
+  transformWithDefaultNoAttributes,
+  transformWithDefaultBuildWith,
+  transformWithDefaultBuildWithNoAttributes,
+  transformWithDefaultNoMaskErr,
+  extend
 }
