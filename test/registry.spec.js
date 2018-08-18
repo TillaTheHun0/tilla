22209e188d9d3ffa29e399dcd841fa3440f976fd
Tyler Hall <0tillathehun0@gmail.com>
2018-08-17 23:45:09 -0400

    test (registry): Add registry tests.

--- test/registry.spec.js
+++ test/registry.spec.js
@@ -1,5 +1,9 @@
 'use strict'
 
 describe('Registry', () => {
+  const tests = require('./registry')
 
+  it('should register the transformer', tests.registerTransformer)
+  it('should retrieve the transformer', tests.getTransformer)
+  it('should clear the registry', tests.clearRegistry)
 })
---
+++ test/registry/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import { expect } from 'chai'
+
+import { registry, Transformer } from '../../src'
+
+function registerTransformer () {
+  let transformer = new Transformer({})
+
+  registry.register('key', transformer)
+
+  expect(registry.registry['key']).to.be.equal(transformer)
+}
+
+function getTransformer () {
+  let transformer = new Transformer({})
+
+  registry.register('key', transformer)
+
+  expect(registry.getTransformer('key')).to.be.equal(transformer)
+}
+
+function clearRegistry () {
+  let transformer = new Transformer({})
+
+  registry.register('key', transformer)
+
+  expect(registry.registry['key']).to.be.equal(transformer)
+
+  registry.clear()
+
+  expect(registry.registry).to.be.eql({})
+}
+
+export {
+  registerTransformer,
+  getTransformer,
+  clearRegistry
+}
