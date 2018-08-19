577f82d0b4fb4444c827a719b408f1a23809bc37
Tyler Hall <0tillathehun0@gmail.com>
2018-08-18 22:43:14 -0400

    test (FieldMapperDelegate): add FieldMapperDelegate tests.

---
+++ test/field.mapper.delegate.spec.js
@@ -0,0 +1,20 @@
+'use strict'
+
+describe('FieldMapperDelegate', () => {
+  const tests = require('./fieldMapperDelegate')
+
+  it('should set and reset the always flag', tests.always)
+  it('should set all the permission lvls and reset flag', tests.alwaysSetsAllLvls)
+  it('should set each permission lvl to passthrough and clear cur permission lvl', tests.passthroughAlways)
+  it('should set each permission lvl to the custom builder and clear cur permission lvl', tests.buildWithAlways)
+  it('should set the is List flag', tests.asList)
+  it('should set all permission lvls', tests.checkAlways)
+  it('should set lower permissions to null', tests.restrict)
+  it('should transform the field on the object', tests.transformAtLvl)
+  it('should not transform the field and return undefined', tests.transformBelowLvl)
+  it('should set the cur permission lvl', tests.when)
+  it('should set the cur permission lvl and restriction', tests.restrictTo)
+  it('should set the permission ranking on the prototype', tests.setPermissionRanking)
+  it('should build the new permission API on the prototype', tests.buildPermissionMethods)
+  it('should throw error if old API is used when permission ranking is provided', tests.oldApiThrowsErr)
+})
---
+++ test/fieldMapperDelegate/index.js
@@ -0,0 +1,190 @@
+'use strict'
+
+import { expect } from 'chai'
+import { FieldPermissionLvl } from '../../src'
+import { PassthroughFieldMapper, CustomFieldMapper } from '../../src/fieldMapper'
+import { FieldMapperDelegate } from '../../src/field.mapper.delegate'
+
+function always () {
+  let delegate = new FieldMapperDelegate('woop')
+
+  delegate.always()
+
+  expect(delegate._always).to.be.equal(true)
+
+  delegate.passthrough()
+
+  expect(delegate._always).to.be.equal(null)
+}
+
+function alwaysSetsAllLvls () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+
+  fMDelegate.always().passthrough()
+
+  fMDelegate.permissionRanking.forEach((p) => {
+    expect(fMDelegate.delegate[p]).to.not.be.equal(null)
+    expect(fMDelegate.delegate[p]).to.not.be.equal(undefined)
+  })
+}
+
+function passthroughAlways () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+
+  fMDelegate.always().passthrough()
+
+  fMDelegate.permissionRanking.forEach((p) => {
+    expect(fMDelegate.delegate[p] instanceof PassthroughFieldMapper).to.be.equal(true)
+  })
+
+  expect(fMDelegate.curPermissionLvl).to.be.equal(null)
+}
+
+function buildWithAlways () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+  let builder = () => {
+    console.log('builder')
+  }
+
+  fMDelegate.always().buildWith(builder)
+
+  fMDelegate.permissionRanking.forEach((p) => {
+    expect(fMDelegate.delegate[p] instanceof CustomFieldMapper).to.be.equal(true)
+  })
+
+  expect(fMDelegate.curPermissionLvl).to.be.equal(null)
+}
+
+function asList () {
+  let fMDelegate = new FieldMapperDelegate('woop').asList()
+  expect(fMDelegate.isList).to.be.equal(true)
+}
+
+function checkAlways () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+
+  fMDelegate.always().passthrough()
+
+  fMDelegate.permissionRanking.forEach((p) => {
+    expect(fMDelegate.delegate[p] instanceof PassthroughFieldMapper).to.be.equal(true)
+  })
+
+  expect(fMDelegate._always).to.be.equal(null)
+}
+
+function restrict () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+
+  let index = fMDelegate.permissionRanking.indexOf(FieldPermissionLvl.PRIVATE)
+  fMDelegate.restrictToPrivate().passthrough()
+
+  fMDelegate.permissionRanking.forEach((p, i) => {
+    if (i < index) {
+      expect(fMDelegate.delegate[p]).to.be.equal(null)
+      return
+    }
+    expect(fMDelegate.delegate[p] instanceof PassthroughFieldMapper).to.be.equal(true)
+  })
+}
+
+function transformAtLvl () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+  let obj = {
+    woop: 200
+  }
+
+  fMDelegate.restrictToPrivate().passthrough()
+
+  fMDelegate.transform(FieldPermissionLvl.PRIVATE, obj).then((value) => {
+    expect(value).to.be.equal(200)
+  })
+}
+
+function transformBelowLvl () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+  let obj = {
+    woop: 200
+  }
+
+  fMDelegate.restrictToPrivate().passthrough()
+
+  fMDelegate.transform(FieldPermissionLvl.PUBLIC, obj).then((value) => {
+    expect(value).to.be.equal(undefined)
+  })
+}
+
+function when () {
+  let fMDelegate = new FieldMapperDelegate('woop').whenPrivate()
+
+  expect(fMDelegate.curPermissionLvl).to.be.equal(FieldPermissionLvl.PRIVATE)
+}
+
+function restrictTo () {
+  let fMDelegate = new FieldMapperDelegate('woop')
+
+  let index = fMDelegate.permissionRanking.indexOf(FieldPermissionLvl.PRIVATE)
+  fMDelegate.restrictToPrivate()
+
+  expect(fMDelegate.curPermissionLvl).to.be.equal(FieldPermissionLvl.PRIVATE)
+  expect(index).to.be.equal(fMDelegate.restriction)
+}
+
+function setPermissionRanking () {
+  let permissions = ['low', 'medium', 'high']
+
+  let fMDelegate = new FieldMapperDelegate('woop', permissions)
+
+  expect(fMDelegate.permissionRanking).has.members(permissions)
+}
+
+function buildPermissionMethods () {
+  let permissions = ['low', 'medium', 'high']
+
+  let fMDelegate = new FieldMapperDelegate('woop', permissions)
+
+  let capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1)
+  }
+
+  permissions.forEach((p) => {
+    let m = fMDelegate[`restrictTo${capitalize(p)}`]
+    expect(m).to.not.be.equal(null)
+    expect(m).to.not.be.equal(undefined)
+  })
+
+  permissions.forEach((p) => {
+    let m = fMDelegate[`when${capitalize(p)}`]
+    expect(m).to.not.be.equal(null)
+    expect(m).to.not.be.equal(undefined)
+  })
+}
+
+function oldApiThrowsErr () {
+  try {
+    let permissions = ['low', 'medium', 'high']
+
+    let fMDelegate = new FieldMapperDelegate('woop', permissions)
+
+    fMDelegate.whenPrivate()
+    throw new Error('Should have thrown error using old API')
+  } catch (err) {
+    expect(err).to.not.be.equal(null)
+  }
+}
+
+export {
+  always,
+  alwaysSetsAllLvls,
+  passthroughAlways,
+  buildWithAlways,
+  asList,
+  checkAlways,
+  restrict,
+  transformAtLvl,
+  transformBelowLvl,
+  when,
+  restrictTo,
+  setPermissionRanking,
+  buildPermissionMethods,
+  oldApiThrowsErr
+}
