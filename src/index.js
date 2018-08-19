59fe008d15b6af7f06f62a7cdf2bf02a4b82f97a
Tyler Hall <0tillathehun0@gmail.com>
2018-08-18 22:42:18 -0400

    refactor (base): BREAKING CHANGE: remove exposed setPermission api.
    This is handled at the instant lvl, so no longer needs to be here.

--- src/index.js
+++ src/index.js
@@ -4,24 +4,17 @@ import 'babel-polyfill'
 
 import { TransformerRegistry } from './transformer.registry'
 import * as FieldMapper from './fieldMapper'
-import { FieldMapperDelegate } from './field.mapper.delegate'
 import { Transformer } from './transformer'
 import { PermissionRanking, FieldPermissionLvl } from './permission'
 import { utils } from './utils'
-
-let setPermissionRanking = (ranking) => {
-  FieldMapperDelegate.prototype.setPermissionRanking(ranking)
-}
 
 const registry = new TransformerRegistry()
 
 export {
   FieldMapper,
-  FieldMapperDelegate,
   Transformer,
   PermissionRanking,
   FieldPermissionLvl,
   utils,
-  registry,
-  setPermissionRanking
+  registry
 }
