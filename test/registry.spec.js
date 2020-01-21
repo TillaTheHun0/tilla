f4661e6bff80477055e20126d9750cdaebcc3bb0
Tyler Hall <0tillathehun0@gmail.com>
2020-01-20 21:36:27 -0500

    chore: remove old dot-chain based API
    BREAKING CHANGE: This removes the v1 dot-chaining based API to make way for the new functional based API

--- test/registry.spec.js
+++
@@ -1,8 +0,0 @@
-
-describe('Registry', () => {
-  const tests = require('./registry')
-
-  it('should register the transformer', tests.registerTransformer)
-  it('should retrieve the transformer', tests.getTransformer)
-  it('should clear the registry', tests.clearRegistry)
-})
